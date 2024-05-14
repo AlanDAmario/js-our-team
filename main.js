3ab6bfa78825161ab13482f5ac7e6d0da85f17a3
AlanDAmario <vega93@live.it>
2024-05-14 16:28:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+// CREAZIONE ARRAY CON OGGETTI, MILESTONE 0
+const members = [
+    {
+        firstname: 'Wayne',
+        lastname: 'Barnett',
+        role: 'Founder & CEO',
+        photo: 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        firstname: 'Angela',
+        lastname: 'Caroll',
+        role: 'Chief Editor',
+        photo: 'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        firstname: 'Walter',
+        lastname: 'Gordon',
+        role: 'Office Manager',
+        photo: 'img/walter-gordon-office-manager.jpg'
+    },
+    {
+        firstname: 'Angela',
+        lastname: 'Lopez',
+        role: 'Social Media Manager',
+        photo: 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        firstname: 'Scott',
+        lastname: 'Estrada',
+        role: 'Developer',
+        photo: 'img/scott-estrada-developer.jpg'
+    },
+    {
+        firstname: 'Barbara',
+        lastname: 'Ramos',
+        role: 'Graphic Designer',
+        photo: 'img/barbara-ramos-graphic-designer.jpg'
+    },
+];
