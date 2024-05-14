ea727ac1f5d5de668e5841ffc5f6b61295279090
AlanDAmario <vega93@live.it>
2024-05-14 17:38:46 +0200

    [UPD] milestone 2

--- main.js
+++ main.js
@@ -7,30 +7,35 @@ const members = [
         role: 'Founder & CEO',
         photo: 'img/wayne-barnett-founder-ceo.jpg'
     },
+
     {
         firstname: 'Angela',
         lastname: 'Caroll',
         role: 'Chief Editor',
         photo: 'img/angela-caroll-chief-editor.jpg'
     },
+
     {
         firstname: 'Walter',
         lastname: 'Gordon',
         role: 'Office Manager',
         photo: 'img/walter-gordon-office-manager.jpg'
     },
+
     {
         firstname: 'Angela',
         lastname: 'Lopez',
         role: 'Social Media Manager',
         photo: 'img/angela-lopez-social-media-manager.jpg'
     },
+
     {
         firstname: 'Scott',
         lastname: 'Estrada',
         role: 'Developer',
         photo: 'img/scott-estrada-developer.jpg'
     },
+
     {
         firstname: 'Barbara',
         lastname: 'Ramos',
@@ -46,3 +51,24 @@ for (let i = 0; i < members.length; i++) {
     console.log('Ruolo', members[i].role);
     console.log('Foto', members[i].photo);
 }
+
+//STAMPARE SU HTML, Milestone 2
+
+function printCard(functionMembers) {
+    const ul = document.querySelector('.list');
+    for (let i = 0; i < functionMembers.length; i++) {
+        const currentobj = functionMembers[i];
+        const li = document.createElement('li');
+        const fName = document.createElement('h2');
+        const lName = document.createElement('h2');
+        const pRole = document.createElement('p');
+        const photo = document.createElement('p')
+        fName.append(currentobj.firstname);
+        lName.append(currentobj.lastname);
+        pRole.append(currentobj.role);
+        photo.append(currentobj.photo);
+        li.append(fName, lName, pRole, photo);
+        ul.append(li);
+    }
+}
+printCard(members);
